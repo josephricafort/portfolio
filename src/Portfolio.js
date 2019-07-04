2bb91f2119c18592c94c719436699e55de801c2e
josephricafort <josephricafortjr@gmail.com>
2019-07-05 00:14:18 +0800

    Initial commit

---
+++ src/Portfolio.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className="Portfolio">
+      <header className="Portfolio-header">
+      </header>
+    </div>
+  );
+}
+
+export default Portfolio;
