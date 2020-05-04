3fd4a9589b132fb2f8d8dbd0dec41f62f2473262
josephricafort <josephricafortjr@gmail.com>
2020-05-05 04:59:24 +0800

    ST link fix

--- src/components/About/Experience.js
+++ src/components/About/Experience.js
@@ -3,42 +3,38 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-    // flex: 1 1 360px;
-    // max-width: 480px;
-    // // padding-right: 60px;
+  // flex: 1 1 360px;
+  // max-width: 480px;
+  // // padding-right: 60px;
 `;
 
 const Experience = () => {
-    return (
-        <Container>
-            <h2>Who I worked with</h2>
-            <p>
-                <strong>
-                    <a href="https://interactivethings.com">
-                    Data Visualization Designer (Present)
-                    </a>
-                </strong>
-                <br />
-                <italic>The Straits Times, Singapore</italic>
-            </p>
-            <p>
-                <strong>
-                    <a href="https://interactivethings.com">
-                    Interaction Design Intern
-                    </a>
-                </strong>
-                <br />
-                <italic>Interactive Things, Zürich, Switzerland</italic>
-            </p>
-            <p>
-                <strong>
-                    Test Development Engineer
-                </strong>
-                <br />
-                <italic>Analog Devices, General Trias, Cavite, Philippines</italic>
-            </p>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <h2>Who I worked with</h2>
+      <p>
+        <strong>
+          <a href="https://straitstimes.com">
+            Data Visualization Designer (Present)
+          </a>
+        </strong>
+        <br />
+        <italic>The Straits Times, Singapore</italic>
+      </p>
+      <p>
+        <strong>
+          <a href="https://interactivethings.com">Interaction Design Intern</a>
+        </strong>
+        <br />
+        <italic>Interactive Things, Zürich, Switzerland</italic>
+      </p>
+      <p>
+        <strong>Test Development Engineer</strong>
+        <br />
+        <italic>Analog Devices, General Trias, Cavite, Philippines</italic>
+      </p>
+    </Container>
+  );
+};
 
 export default Experience;
